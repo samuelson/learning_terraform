a7c28badf260b4187657b37aad04a145094e7b94
mobgtr <mobgtr@gmail.com>
2023-11-24 17:48:14 -0300

    test-secgroup

--- main.tf
+++ main.tf
@@ -42,6 +42,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   to_port     = 80
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.blog.id
 }
 
@@ -52,6 +53,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   to_port     = 443
   protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
+
   security_group_id = aws_security_group.blog.id
 }
 
@@ -62,5 +64,6 @@ resource "aws_security_group_rule" "blog_everything_out" {
   to_port     = 0
   protocol    = "-1"
   cidr_blocks = ["0.0.0.0/0"]
+  
   security_group_id = aws_security_group.blog.id
 }
